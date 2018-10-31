d83ddf8638d659d3396896640a00462ff5603b7e
happydarrel <happy_darrel@163.com>
2018-10-31 20:20:11 +0800

    cc

---
+++ Test.c
@@ -0,0 +1,46 @@
+//常用缩写 
+init      <initiate initialization> 初始化
+
+def        <define>                  定义
+
+idx        <index>                   索引 指针
+
+buf        <buffer>                  缓冲区
+
+msg        <message>                 消息 报文
+
+data                                 数据
+
+param      <parameter>               参数
+
+update                               更新   
+
+date                                 日期
+
+size                                 大小
+
+add        <address>                 地址
+
+_t         <type>                    类型
+
+exe        <execute>                 执行
+
+query                                查询-询问
+
+restore                              恢复
+
+route                                按某种方式发送
+
+register                             注册     
+
+failed                               失败
+
+array                                阵列
+
+module                               模块
+
+len          <length>                长度
+
+parse                                解析
+
+process                              处理
